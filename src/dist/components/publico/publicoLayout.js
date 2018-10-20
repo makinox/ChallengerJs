8bce7702518451f8c47ad369d930358ab354d011
Jesús Bossa <me@jesusbossa.com>
2018-10-20 14:29:27 -0500

    [Add] Last page of challengers

---
+++ src/dist/components/publico/publicoLayout.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import './publicoLayout.css'
+
+export default () => (
+  <h1>We are the publico</h1>
+)
---
+++ src/dist/pages/publico.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+import PublicoLayout from '../components/publico/publicoLayout'
+
+export default () => <PublicoLayout />
