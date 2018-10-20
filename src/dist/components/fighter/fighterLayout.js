f4f8baed0c44b81bc516f280ae929e347ed2fd54
Jesús Bossa <me@jesusbossa.com>
2018-10-20 14:14:03 -0500

    [Add] Challenger pages initialized

---
+++ src/dist/components/fighter/fighterLayout.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import './fighterLayout.css'
+
+export default () => (
+  <h1>Fight right now</h1>
+  )
---
+++ src/dist/pages/challenger.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import ChallengerLayout from '../components/challenger/challengerLayout'
+
+export default () => <ChallengerLayout />
