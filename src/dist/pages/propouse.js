e30fe9722bea38879f4301d334d613520fbe38bd
Rafel Villarreal <rvillarreal416@gmail.com>
2018-10-21 00:31:36 -0500

    [improvement]  emit challenge creating

--- src/dist/pages/propouse.js
+++ src/dist/pages/propouse.js
@@ -1,14 +1,29 @@
 import React from 'react'
 import PropouseLayout from '../components/propouse/propouseLayout'
+import { socket } from '../utils/socket/socket'
 
 export default class Propouse extends React.Component {
 
+  componentDidMount () {
+    socket.on('challenge', (data) => {
+      console.log(data)
+    })
+  }
+
   onSubmit = (e) => {
+    console.log('submit')
     e.preventDefault()
-    console.log(e.target.title.value)
-    console.log(e.target.description.value)
-    console.log(e.target.input.value)
-    console.log(e.target.ouput.value)
+    const title = e.target.title.value
+    const description = e.target.description.value
+    const input = e.target.input.value
+    const ouput = e.target.ouput.value
+
+    socket.emit('create', {
+      title,
+      description,
+      input,
+      ouput
+    })
   }
 
   render () {
